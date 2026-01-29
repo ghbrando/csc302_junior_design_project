edc6f3af7fc9465be78b8a1447280617b4c97dd2
ghbrando <155781260+ghbrando@users.noreply.github.com>
2026-01-28 21:02:09 -0800

    Update demo.py
    updated demo.py to implement greeting users for user story X

--- demo.py
+++ demo.py
@@ -1,2 +1,8 @@
-#print statement to print hello world
-print("Hello world!")
+def printName(name):
+    return "Hello " + name + "!"
+
+def main():
+    print(printName("Brandon"))
+    
+if __name__ == "__main__":
+    main()
